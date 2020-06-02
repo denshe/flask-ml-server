8c1c204f2e93c888fdc3d868bd0172aa6292fb0c
Denis <denshe@gmail.com>
2020-06-02 12:23:27 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import config
 
 # get predictions for the first batch
 r = requests.post(config.MODEL_RUN['url'], json = pd.read_json('batch_500.json').to_json()) 
